b388e8c1f82953ad11861dd8962740494c5d013e
coral <jonas@jonasbengtson.se>
2019-05-12 15:01:27 -0400

    BENIS

---
+++ Assets/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using WebSocketSharp;
+
+public class Connection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("benis");
+        var ws = new WebSocket("ws://localhost:9000/stream");
+        
+            ws.OnMessage += (sender, e) =>
+                Debug.Log(e.Data);
+
+            ws.Connect();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
